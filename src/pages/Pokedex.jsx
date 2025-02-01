df099fce73f910c503467c4078adc2acf93118be
byRandom <tomasperezv11@gmail.com>
2025-02-01 05:54:11 +0100

    fix: correct import path for PokemonList component

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PokemonList } from '../Components/PokemonList'
+import { PokemonList } from '../components/PokemonList'
 
 export const Pokedex = ({pokemonListState}) => {
   return (
